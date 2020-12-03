1fe5a5b12fb888d0b3c4b6ac42aa226e41b9b69d
nicholasmeier <nimtizmeier@gmail.com>
2020-12-03 18:51:50 -0500

    d3p2, first attempt

---
+++ day3/day3_part2.py
@@ -0,0 +1,29 @@
+# Goal : find the trees
+import sys
+def Main():
+    f = open("input_d3.txt", "r")
+    slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+    
+    mtn = f.read().splitlines()
+    i = 0
+    j = 0
+
+    t_cnt=1
+    for s in slopes:
+        cnt = 0
+        s_x = s[0]
+        s_y = s[1]
+        while True:
+            j += s_y
+            i += s_x
+            if j >= len(mtn):
+                break
+            if (i >= len(mtn[j])):
+                i = 0+(i-len(mtn[j]))
+            if mtn[j][i] == '#':         
+                cnt += 1
+        t_cnt *= cnt
+    print(t_cnt)
+
+if __name__ == '__main__':
+    Main()
