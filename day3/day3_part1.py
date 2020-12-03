b6af2eb56ef886419c9e09e75b0a0845ef66c93e
nicholasmeier <nimtizmeier@gmail.com>
2020-12-03 18:43:40 -0500

    working solution

---
+++ day3/day3_part1.py
@@ -0,0 +1,29 @@
+# Goal : find the trees
+import sys
+def Main():
+    f = open("input_d3.txt", "r")
+    s_x = 3
+    s_y = 1
+    
+    mtn = f.read().splitlines()
+    i = 0
+    j = 0
+
+    cnt = 0
+    while True:
+        j += s_y
+        i += s_x
+        if j >= len(mtn):
+            break
+        if (i >= len(mtn[j])):
+            i = 0+(i-len(mtn[j]))
+        print(mtn[j])
+        print(mtn[j][i])
+        print("i: " + str(i))
+        print("j: " + str(j))
+        if mtn[j][i] == '#':         
+            cnt += 1
+    print(cnt)
+
+if __name__ == '__main__':
+    Main()
